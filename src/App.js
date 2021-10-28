2ebeb291a08f2c33ab14a0ebc59ce45b4b89cb03
Radoslav Tsanev <tsanev96@gmail.com>
2021-10-29 00:10:37 +0300

    adding routing

--- src/App.js
+++ src/App.js
@@ -2,16 +2,24 @@ import React from "react";
 import { Provider } from "react-redux";
 import { Albums } from "./components/Albums";
 import configureStore from "./store/configureStore";
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+import { AlbumPhotos } from "./components/AlbumPhotos";
 
 const store = configureStore();
 
 const App = () => {
   return (
-    <Provider store={store}>
-      <div className="App">
-        <Albums />
-      </div>
-    </Provider>
+    <BrowserRouter>
+      <Provider store={store}>
+        <div className="App">
+          <Switch>
+            <Route path="/albums/:id" component={AlbumPhotos} />
+            <Route path="/albums" component={Albums} />
+            <Route path="/" exact component={Albums} />
+          </Switch>
+        </div>
+      </Provider>
+    </BrowserRouter>
   );
 };
 
