e8b80e7c84c7ac5c6db5cef67fd53ff94191f858
Radoslav Tsanev <tsanev96@gmail.com>
2021-10-29 11:36:03 +0300

    ussing mui cards for the photos

--- src/components/AlbumPhotos.js
+++ src/components/AlbumPhotos.js
@@ -1,3 +1,13 @@
+import {
+  Button,
+  Card,
+  CardActionArea,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Grid,
+  Typography,
+} from "@mui/material";
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router";
@@ -15,25 +25,50 @@ export const AlbumPhotos = () => {
     (album) => album.id === paramsAlbumId
   );
   const photos = currentAlbumIndex >= 0 ? albums[currentAlbumIndex].photos : [];
-
+  console.log(photos);
   useEffect(() => {
     dispatch(loadPhotos());
   }, []);
 
   return (
-    <div>
+    <>
       {photos.length ? (
-        <>
+        <Grid
+          container
+          direction="column"
+          justifyContent="center"
+          alignItems="center"
+        >
           <div>Album: {paramsAlbumId}</div>
-          <div>
+          <>
             {photos.map((photo) => (
-              <img key={photo.id} src={photo.url} alt={photo.id} />
+              // <img key={photo.id} src={photo.url} alt={photo.id} />
+              <Card key={photo.id} sx={{ maxWidth: 345 }}>
+                <CardActionArea>
+                  <CardMedia
+                    component="img"
+                    height="140"
+                    image={photo.url}
+                    alt="green iguana"
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                      {photo.title}
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+                <CardActions>
+                  <Button size="small" color="primary">
+                    fav
+                  </Button>
+                </CardActions>
+              </Card>
             ))}
-          </div>
-        </>
+          </>
+        </Grid>
       ) : (
         <div>This album does not exist</div>
       )}
-    </div>
+    </>
   );
 };
