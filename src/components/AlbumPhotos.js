1df060bcd6f55b8d4eacc67dbd33a8a1d10dd800
Radoslav Tsanev <tsanev96@gmail.com>
2021-10-29 11:20:01 +0300

    adding check if album exist at first place

--- src/components/AlbumPhotos.js
+++ src/components/AlbumPhotos.js
@@ -1,27 +1,30 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router";
 import { loadPhotos, selectAlbums } from "../store/photos";
 
 export const AlbumPhotos = () => {
   const dispatch = useDispatch();
+  const params = useParams();
 
   const albums = useSelector(selectAlbums());
 
-  const params = useParams();
-  const albumId = Number(params.id);
+  const paramsAlbumId = Number(params.id);
 
-  const currentAlbumId = albums.findIndex((album) => album.id === albumId);
-  const photos = albums[currentAlbumId].photos;
+  const currentAlbumIndex = albums.findIndex(
+    (album) => album.id === paramsAlbumId
+  );
+  const photos = currentAlbumIndex >= 0 ? albums[currentAlbumIndex].photos : [];
 
   useEffect(() => {
     dispatch(loadPhotos());
   }, []);
+
   return (
     <div>
-      {currentAlbumId >= 0 ? (
+      {photos.length ? (
         <>
-          <div>Album: {currentAlbumId}</div>
+          <div>Album: {paramsAlbumId}</div>
           <div>
             {photos.map((photo) => (
               <img key={photo.id} src={photo.url} alt={photo.id} />
@@ -29,7 +32,7 @@ export const AlbumPhotos = () => {
           </div>
         </>
       ) : (
-        <div>not existing album</div>
+        <div>This album does not exist</div>
       )}
     </div>
   );
